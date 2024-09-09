310145343f54d0f1cfff40baa2ed290bc9a3e973
Laurie-Multiverse <laurie.corrin@multiverse.io>
2024-09-09 09:12:14 -0700

    added methods

--- classes/Person.js
+++ classes/Person.js
@@ -4,6 +4,18 @@ class Person {
         this.type = type
         this.location = location || [0, 0]
     }
+
+    introduce() {
+        return `Hello, my name is ${this.name} and I'm a ${this.type}.`
+    }
+
+    updateX(dx) {
+        this.location[0] += dx;
+    }
+
+    updateY(dy) {
+        this.location[1] += dy;
+    }
 }
 
 module.exports = Person
--- main.js
+++ main.js
@@ -6,5 +6,13 @@ function main() {
     console.log(person.name)
     console.log(person.type)
     console.log(person.location)
+    console.log(person.introduce())
+
+    const person2 = new Person("Ava", "Warrior")
+    console.log(person2)
+    console.log(person2.introduce())
+    person2.updateX(3);
+    person2.updateY(-1);
+    console.log(person2)
 }
 main()
